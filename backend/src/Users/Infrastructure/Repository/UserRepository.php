425eabf6ad94249f9ab09bac4b8628e39515ad52
Andrey Arzamaskov <arzamaskov@gmail.com>
2025-03-01 11:01:56 +0400

    create User entity: entity, repository, factory, tests

--- backend/src/Users/Infrastructure/Repository/UserRepository.php
+++ backend/src/Users/Infrastructure/Repository/UserRepository.php
@@ -2,8 +2,33 @@
 
 namespace App\Users\Infrastructure\Repository;
 
+use App\Users\Domain\Entity\User;
 use App\Users\Domain\Repository\UserRepositoryInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
-class UserRepository implements UserRepositoryInterface
+/**
+ * @extends ServiceEntityRepository<object>
+ */
+class UserRepository extends ServiceEntityRepository implements UserRepositoryInterface
 {
+    private EntityManagerInterface $_em;
+
+    public function __construct(ManagerRegistry $registry, EntityManagerInterface $entityManager)
+    {
+        parent::__construct($registry, User::class);
+        $this->_em = $entityManager;
+    }
+
+    public function add(User $user): void
+    {
+        $this->_em->persist($user);
+        $this->_em->flush();
+    }
+
+    public function findByUlid(string $ulid): User
+    {
+        return $this->find($ulid);
+    }
 }
