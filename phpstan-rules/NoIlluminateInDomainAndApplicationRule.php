c106325035764bb79295363b34af280afcabbf30
Andrey Arzamaskov <arzamaskov@gmail.com>
2025-10-23 23:18:38 +0400

    fix: добавлена директория phpstan-rules в анализируемые пути
    - Исправлено предупреждение PHPStan о некорректном кеше
    - Теперь phpstan-rules включена в paths для анализа
    - Кеш будет автоматически обновляться при изменении правил

--- phpstan-rules/NoIlluminateInDomainAndApplicationRule.php
+++ phpstan-rules/NoIlluminateInDomainAndApplicationRule.php
@@ -11,10 +11,10 @@ use PHPStan\Rules\RuleErrorBuilder;
 
 /**
  * Правило: запрет использования Illuminate\* в Domain и Application слоях
- * 
+ *
  * Это архитектурное ограничение для DDD (Domain-Driven Design).
  * Domain и Application слои должны быть независимы от фреймворка.
- * 
+ *
  * @implements Rule<Node\Stmt\Use_>
  */
 class NoIlluminateInDomainAndApplicationRule implements Rule
@@ -27,18 +27,18 @@ class NoIlluminateInDomainAndApplicationRule implements Rule
     public function processNode(Node $node, Scope $scope): array
     {
         $file = $scope->getFile();
-        
+
         // Проверяем только файлы в Domain и Application слоях
         if (!preg_match('#/app/[^/]+/(Domain|Application)/#', $file, $matches)) {
             return [];
         }
-        
+
         $layer = $matches[1];
         $errors = [];
-        
+
         foreach ($node->uses as $use) {
             $useName = $use->name->toString();
-            
+
             // Проверяем use Illuminate\*
             if (str_starts_with($useName, 'Illuminate\\')) {
                 $errors[] = RuleErrorBuilder::message(
@@ -56,8 +56,7 @@ class NoIlluminateInDomainAndApplicationRule implements Rule
                 ->build();
             }
         }
-        
+
         return $errors;
     }
 }
-
